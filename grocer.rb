576f12341562c1c3f293f537ec389a1f4061b833
David Little <littledavid175@gmail.com>
2019-07-06 19:05:42 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -41,7 +41,7 @@ def checkout(cart, coupons)
   app_cou = apply_coupons(cart, coupons)
   app_cle = apply_clearance(cart)
   total = 0.00
-  cart_with_discounts_applied.keys.each do |item|
+ app_cle.keys.each do |item|
     total += cart_with_discounts_applied[item][:price]*cart_with_discounts_applied[item][:count]
 end
   if total > 100.00
