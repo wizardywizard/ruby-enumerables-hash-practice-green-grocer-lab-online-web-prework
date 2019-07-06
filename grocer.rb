5471658e2ed0ae27e17c163829860484cca9a72c
David Little <littledavid175@gmail.com>
2019-07-06 17:43:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -31,6 +31,7 @@ def apply_clearance(cart)
   cart.each{|clearance|
     if cart.keys.include?(clearance[:item])
       if clearance[:clearance] === cart[clearance[:item]][:clearance]
+        
   }
 end
 
